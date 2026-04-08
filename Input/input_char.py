da4e6c564da4dcaf5975726d4509128c45fe3d10
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-09 00:27:52 +0530

    Taking character as an input from the user

---
+++ Input/input_char.py
@@ -0,0 +1,7 @@
+# Here we will take character as an input from the user
+
+# Here user can enter any size but this variable will recieve only one character
+char1 = input("Enter the first character : ")[0]
+char2 = input("Enter the second character : ")[0]
+
+print(char1 + ' ' + char2)
