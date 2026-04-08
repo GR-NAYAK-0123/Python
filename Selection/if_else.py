07144f3f02b3588a2c334b027b6a13e57d7bbdb7
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-09 01:05:39 +0530

    Learned about "if-else" statement with an example

---
+++ Selection/if_else.py
@@ -0,0 +1,10 @@
+
+# Here I will take an input (age) from the user and print if the user is eligible for vote or not
+# For this i will use if-else statement
+
+age = int(input("Enter your age : "))
+
+if age >= 18 :
+    print("You are eligible for vote :)")
+else :
+    print("You are not eligible for vote ):")
