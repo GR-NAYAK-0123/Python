fcc875586dbd0a8f675e79f6ce652c98e2b53335
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-08 22:14:02 +0530

    Perform swapping operation

---
+++ Function/swap_two_number.py
@@ -0,0 +1,12 @@
+
+def swap(a, b):
+    #Here swapping means assigning b value to a and vice-versa
+    return b, a
+
+
+a = 10
+b = 20;
+a, b = swap(a, b)
+
+print("value of a : ",a)
+print("value of b : ",b)
