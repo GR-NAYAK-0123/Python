1a06bcbd6ff07d9b45a2c10d0dffa1fd98aa877f
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-09 00:51:15 +0530

    Learned about "if" statement

---
+++ Selection/if.py
@@ -0,0 +1,7 @@
+
+# Here I will take one number from the user and check the number is even or odd
+
+number = int(input("Enter a natural number : "))
+
+if number % 2 == 0:
+    print("The number ",number," is even")
