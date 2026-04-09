1819ecc4ae4579a43b8c5aa374721423ecb4e578
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-10 02:15:18 +0530

    Here I used to traverse and print all the values of the list byusing "for" loop

---
+++ Iteration/For/for_2.py
@@ -0,0 +1,10 @@
+
+# Here I am going to traverse an entire list by the help for "for" Loop
+# While traversing the list, I am going to print the values
+
+list = ['Radha', 1, 2, 3, 4, 5, 'Krishna']
+
+for i in list :
+    print(i)
+
+print("Operation End :)")
