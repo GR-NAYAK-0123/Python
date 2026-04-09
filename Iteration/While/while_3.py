625b2849a509d1fe4d47c2a1b9392e425491173a
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-10 02:04:31 +0530

    Here I traversed the entire list byusing while loop

---
+++ Iteration/While/while_3.py
@@ -0,0 +1,13 @@
+
+# Here I am going to traverse an entire list by using while loop
+# While traversing the list simultaneously I am going to print the values of each index
+
+list = ['Radha', 1, 2, 3, 4, 5, 'Krishna']
+
+i = 0;
+
+while i < len(list) :
+    print(list[i])
+    i += 1
+
+print("Operation end !!!")
