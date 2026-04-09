dca3f5dbadcfd093dcc5db361c884c38c1262934
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-10 01:54:30 +0530

    Using "while" loop, I have printed all the even and odd numbers between 1 and 10

---
+++ Iteration/While/while_2.py
@@ -0,0 +1,15 @@
+
+# Here I am going to print all the even numbers between 1 and 20
+# For this operation i going to use while loop
+
+number = 1
+
+while number <= 10 :
+    if number % 2 == 0 :
+        print(number, " is even number")
+        number += 1
+    else :
+        print(number, " is odd number")
+        number += 1
+
+print("end !!!")
