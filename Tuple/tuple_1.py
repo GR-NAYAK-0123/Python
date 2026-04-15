4f5019b897b029aaf807556e7349d73fd0e8a806
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-15 12:13:58 +0530

    Learned about "tuple"

---
+++ Tuple/tuple_1.py
@@ -0,0 +1,12 @@
+
+"""Tuple is a data structure in python and it maintains the insertion order means we can returieve the element by using index value
+   and it is immutable means we can not modify it's element, it also accepts duplicate element and also it accepts heterogeneous data,
+   for creating a tuple we need to use () braces
+"""
+
+nums = (1, 4, 6, 8)
+
+# nums[0] = 2   # We can't change it's element
+
+for i in nums:
+    print(i)
