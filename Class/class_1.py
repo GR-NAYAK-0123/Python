0dc5e7b9bc2ac388608c49e0734c1b9c9d4555fd
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-18 02:47:19 +0530

    Learning about the class

---
+++ Class/class_1.py
@@ -0,0 +1,7 @@
+
+class Man:
+    name = "Raja"  #Variables or attributes
+
+print(Man.name)
+obj = Man()       # Creating the object for the class
+print(obj.name)   # By the help of the object we are accessing the attribute
---
+++ Class/class_2.py
@@ -0,0 +1,11 @@
+
+
+class Man:
+    name = "Raja"   # Attribute
+
+    def full_name(self):        # Method
+        print("Raja Nayak")
+
+obj = Man()       # Creating the object of the class Man
+print(obj.name)
+obj.full_name()   # By using the object I am calling the method and attribute
