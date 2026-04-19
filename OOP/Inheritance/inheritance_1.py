016dd3348ff43156535cdf95f77600d93fc44fff
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-19 11:45:23 +0530

    Learned about Inheritance

---
+++ OOP/Inheritance/inheritance_1.py
@@ -0,0 +1,19 @@
+
+class Car:
+    def __init__(self, wheel, speed):
+        self.wheel = wheel
+        self.speed = speed
+    
+    def details(self):
+        print(f"{self.wheel}, {self.speed}")
+
+class Audi(Car):
+    def __init__(self, wheel):
+        self.wheel = wheel
+    
+    # # def details(self):
+    #     print(self.wheel)
+
+
+obj1 = Audi(4)
+# obj1.details()     # We will error here because Audi doesn't have any speed argument
