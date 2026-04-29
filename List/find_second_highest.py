68e0479caf92058f1055a695a5e96a9966ba7c8f
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-04-29 11:51:16 +0530

    Added another approach to get the second hoghest element

--- List/find_second_highest.py
+++ List/find_second_highest.py
@@ -2,15 +2,25 @@
 
 nums = [2, 4, 7, 12, 11, 34, 9]
 
-firstMax = nums[0]
-secondMax = nums[0]
+# firstMax = nums[0]
+# secondMax = nums[0]
 
-for i in range(len(nums)):
-    if nums[i] > firstMax:
-        secondMax = firstMax
-        firstMax = nums[i]
-    elif nums[i] > secondMax:
-        secondMax = nums[i]
+# Efficient approach
+# for i in range(len(nums)):
+#     if nums[i] > firstMax:
+#         secondMax = firstMax
+#         firstMax = nums[i]
+#     elif nums[i] > secondMax:
+#         secondMax = nums[i]
+
+nums.sort()
+first_max = nums[-1]
+second_max = float('-inf')
+
+# Not very efficient approach
+for i in range(len(nums)-1, -1, -1):
+    if nums[i] < first_max:
+        second_max = nums[i]
 
 
-print(f"The largest element in the list is {firstMax} and second largest is {secondMax}")
+print(f"The largest element in the list is {first_max} and second largest is {second_max}")
