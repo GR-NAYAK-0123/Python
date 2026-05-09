6e664973076bd8ff15fe12161c8bd9a736df8ffd
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-05-09 22:50:55 +0530

    Practiced the top k frequent element

--- Questions/Last_minute_practice/top_k_frequent_element.py
+++ Questions/Last_minute_practice/top_k_frequent_element.py
@@ -8,9 +8,10 @@ def top_k_frequent(nums, k):
             d[i] += 1
         else:
             d[i] = 1
-    values = [x for x in d.items()]
-    values.sort(key=lambda x:x[1], reverse=True)
-    return [values[x][0] for x in range(k)]
+
+    values = [i for i in d.items()]
+    values.sort(key=lambda x: x[1], reverse=True)
+    return [values[i][0] for i in range(k)]
 
 nums = [1, 2, 2, 3, 2, 3, 2, 3, 2, 1, 1, 1, 4]
 k = 3
