1a532b2b215ea550f7f2bb9dc98dcb14f7388797
GR-NAYAK-0123 <rajanayak7889@gmail.com>
2026-05-29 08:34:29 +0530

    Recap about "for"

---
+++ Iteration/For/for_3.py
@@ -0,0 +1,7 @@
+
+# I have to print something repeatedly by using "for" loop
+
+for i in [10, 20, 30]:
+    print("Radha")
+
+# It will print the statement 3 times, because the value of i will be (10, 20, 30) respectively
